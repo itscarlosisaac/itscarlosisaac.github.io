eb2f4d87ebd5a9b4d323e2457c0a2545408ba27d
Carlos <itscarlosisaac@gmail.com>
2020-09-09 11:55:34 -0400

    Finished navigation.

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,3 @@
 /* ================
- Template Name: Template Name
- Description: Template Description
- Version: 1.0
- Author: https://themeforest.net/user/seasonsca
+
 ======================= */
-
-
-var slider = tns({
-    container: '.slider',
-    items: 1,
-    speed: 400,
-    loop: true,
-    controls: false,
-    nav: true,
-    navPosition: "bottom",
-    navContainer: ".navContainer"
-});
--- src/js/partials/Slider.js
+++ src/js/partials/Slider.js
@@ -1,3 +1,14 @@
 /* ================
  Section Name: Slider
 ======================= */
+
+var slider = tns({
+    container: '.slider',
+    items: 1,
+    speed: 400,
+    loop: true,
+    controls: false,
+    nav: true,
+    navPosition: "bottom",
+    navContainer: ".navContainer"
+});
