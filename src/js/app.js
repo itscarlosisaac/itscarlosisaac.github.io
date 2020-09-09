76f31e9af77de36af598ae370cabf00a2f8b8cfe
Carlos <itscarlosisaac@gmail.com>
2020-09-09 11:50:18 -0400

    Finished basic site.

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,15 @@
  Version: 1.0
  Author: https://themeforest.net/user/seasonsca
 ======================= */
+
+
+var slider = tns({
+    container: '.slider',
+    items: 1,
+    speed: 400,
+    loop: true,
+    controls: false,
+    nav: true,
+    navPosition: "bottom",
+    navContainer: ".navContainer"
+});
